22f100065a4dd18e084c5665d4906642bc31b970
Will Loughlin <will.loughlin@gmail.com>
2025-05-06 10:43:55 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 morgan.token('body', (request, response) => { 
@@ -9,7 +8,6 @@ morgan.token('body', (request, response) => {
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
 let persons = [
