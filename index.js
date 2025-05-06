37b64b9749e9635bf160b498ca58983d1ceaed0e
Will Loughlin <will.loughlin@gmail.com>
2025-05-06 10:11:45 +0100

    uibuild

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', (request, response) => {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
